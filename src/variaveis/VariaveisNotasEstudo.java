dd2e27c39e7309e508c829cee17cacc8f9f03a69
IanSilvaCertacon <ian.silva@certacon.com.br>
2024-11-08 14:06:50 -0300

    Primeiro commit

---
+++ src/variaveis/VariaveisNotasEstudo.java
@@ -0,0 +1,13 @@
+package variaveis;
+
+public class VariaveisNotasEstudo {
+    public static void main(String[] args) {
+        int dia = 8;
+        int mes = 11;
+        int ano = 2024;
+
+        System.out.println(dia + mes + ano);
+        System.out.println(mes);
+        System.out.println(ano);
+    }
+}
